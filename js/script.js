dda7545587e9b4e8cbcc379fa863b5e2bf253a70
MartinWill93 <82729369+MartinWill93@users.noreply.github.com>
2023-04-02 16:43:43 +0300

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-/* ACTIVE PANEL */
-$(function() {
-  $('.nav-menu-panel [href]').each(function() {
-    if (this.href == window.location.href) {
-      $(this).addClass('active-menu-panel');
-    }
-  });
-});
